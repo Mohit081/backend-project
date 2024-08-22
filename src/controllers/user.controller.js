d028ca2c04e44fc2bd044e9c542532ded5f07a59
Mr_mohit <mohitgiri852004@gmail.com>
2024-08-22 12:58:35 +0530

    get user watch history controller endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -406,6 +406,55 @@ const getUserChannelprofile = asyncHandler(async (req, res) => {
     );
 });
 
+const getUserWatchHistory = asyncHandler(async (req,res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField:"_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project:{
+                    fullName: 1,
+                    userName: 1,
+                    avatat: 1
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner"
+        }
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200 , user[0].watchHistory,"watch history fetched successfully"))
+})
+
+
 export {
   userRegister,
   userLogin,
@@ -416,4 +465,5 @@ export {
   updateAvatar,
   updateCoverImage,
   getUserChannelprofile,
+  getUserWatchHistory 
 };
